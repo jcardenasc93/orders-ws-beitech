5fd031f148a941c506e40088a4cbc95d44ff99f0
jcardenasc93 <camilo.cardenas@grability.com>
2021-03-18 01:22:39 -0500

    Creates project app and defines DB models

---
+++ orders/models.py
@@ -0,0 +1,63 @@
+""" orders models definitions """
+from django.db import models
+
+# Create your models here.
+
+CHAR_MAX_LENGTH = 191
+
+
+class Product(models.Model):
+    """ Product model definition """
+    product_id = models.AutoField(auto_created=True, primary_key=True)
+    name = models.CharField(max_length=CHAR_MAX_LENGTH)
+    product_description = models.CharField(max_length=CHAR_MAX_LENGTH)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'product'
+
+
+class Customer(models.Model):
+    """ Customer model definition """
+    customer_id = models.AutoField(auto_created=True, primary_key=True)
+    name = models.CharField(max_length=CHAR_MAX_LENGTH)
+    email = models.EmailField(max_length=CHAR_MAX_LENGTH)
+
+    class Meta:
+        db_table = 'customer'
+
+
+class CustomerProduct(models.Model):
+    """ CustumerProduct intermediate table definition """
+    customer_product_id = models.AutoField(auto_created=True, primary_key=True)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'customer_product'
+        # Adds compose unique restriction
+        unique_together = (('product_id'), ('customer_id'))
+
+
+class Order(models.Model):
+    """ Order model definition """
+    order_id = models.AutoField(auto_created=True, primary_key=True)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    delivery_address = models.CharField(max_length=CHAR_MAX_LENGTH)
+    total = models.DecimalField(max_digits=15, decimal_places=2)
+
+    class Meta:
+        db_table = 'order'
+
+
+class OrderDetail(models.Model):
+    """ Order detail model definition """
+    order_detail_id = models.AutoField(auto_created=True, primary_key=True)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product_description = models.CharField(max_length=CHAR_MAX_LENGTH)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'order_detail'
