ba0dd8d91e387db4e7dd78dd890de9eefc6d2a39
jcardenasc93 <camilo.cardenas@grability.com>
2021-03-18 23:50:01 -0500

    Updates db tables column names

--- orders/models.py
+++ orders/models.py
@@ -35,8 +35,8 @@ class Customer(models.Model):
 class CustomerProduct(models.Model):
     """ CustumerProduct intermediate table definition """
     customer_product_id = models.AutoField(auto_created=True, primary_key=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, db_column='product_id')
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, db_column='customer_id')
 
     class Meta:
         db_table = 'customer_product'
@@ -47,7 +47,7 @@ class CustomerProduct(models.Model):
 class Order(models.Model):
     """ Order model definition """
     order_id = models.AutoField(auto_created=True, primary_key=True)
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, db_column='customer_id')
     creation_date = models.DateTimeField(auto_now_add=True)
     delivery_address = models.CharField(max_length=CHAR_MAX_LENGTH)
     total = models.DecimalField(max_digits=15, decimal_places=2)
@@ -62,9 +62,9 @@ class Order(models.Model):
 class OrderDetail(models.Model):
     """ Order detail model definition """
     order_detail_id = models.AutoField(auto_created=True, primary_key=True)
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_details')
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_details', db_column='order_id')
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, db_column='product_id')
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, db_column='customer_id')
     product_description = models.CharField(max_length=CHAR_MAX_LENGTH, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
     quantity = models.IntegerField(max_length=999999999)
